33c1739b94be8cddc04cf8676c93bef66f406123
Martha Cash <63068410+mcash8@users.noreply.github.com>
2022-02-22 15:57:59 -0600

    Update PacketTest.m

--- Feed Forward NN/Packet Test/PacketTest.m
+++ Feed Forward NN/Packet Test/PacketTest.m
@@ -1,5 +1,6 @@
 %Generate testing data, similar to above code but only one sequence of
 %integers are generated 
+rng(0); 
 M=16;
 window = 4;
 
@@ -13,30 +14,32 @@ rxfilter = comm.RaisedCosineReceiveFilter(); %rx srrc
 
 
 for nb = [5000, 10000, 50000]
-    LO = dsp.SineWave("Frequency", 1.5e9, "ComplexOutput", true, "SampleRate", (1e-6)/8, "SamplesPerFrame", 8*nb); 
-    y = LO(); %up conversion
-    msg = randi([0 sum(M)-1], nb, 1);
-    symbols = dvbsapskmod(msg, M, 's2x'); 
-    txfilterData = txfilter(symbols); 
-    txData = txfilterData.*y; %upconvert
-    txData = awgnChan(txData);
-    
-    rxData = txData.*conj(y); %down convert
-    rx = rxfilter(rxData); %SRRC filter
-    
-    %constDiagram([symbols, rx]); 
-    
-    d = finddelay(symbols, rx); %need to remove delay before windowing
-    rx = rx((d+1):end); %get rid of filter delay 
-    symbols = symbols(1:(end-d));
-    
-    train_data = makeInputMat(rx,window);
-    train_data = train_data(:,1:end-window);
-    symbols = symbols(1+window:end);
-    target = round([real(symbols) imag(symbols)],5)';
-    
-    train_data = train_data'; 
-    
-    filename_test = sprintf('DVBSAPSK_pred_SRRC_%dpck', nb); 
-    save(filename_test, 'train_data', 'target'); 
+    for i = 1:10
+        LO = dsp.SineWave("Frequency", 1.5e9, "ComplexOutput", true, "SampleRate", (1e-6)/8, "SamplesPerFrame", 8*nb); 
+        y = LO(); %up conversion
+        msg = randi([0 sum(M)-1], nb, 1);
+        symbols = dvbsapskmod(msg, M, 's2x'); 
+        txfilterData = txfilter(symbols); 
+        txData = txfilterData.*y; %upconvert
+        txData = awgnChan(txData);
+        
+        rxData = txData.*conj(y); %down convert
+        rx = rxfilter(rxData); %SRRC filter
+        
+        %constDiagram([symbols, rx]); 
+        
+        d = finddelay(symbols, rx); %need to remove delay before windowing
+        rx = rx((d+1):end); %get rid of filter delay 
+        symbols = symbols(1:(end-d));
+        
+        train_data = makeInputMat(rx,window);
+        train_data = train_data(:,1:end-window);
+        symbols = symbols(1+window:end);
+        target = round([real(symbols) imag(symbols)],5)';
+        
+        train_data = train_data'; 
+        
+        filename_test = sprintf('DVBSAPSK_pred_SRRC_%dpck_%d', [nb, i]); 
+        save(filename_test, 'train_data', 'target'); 
+    end 
 end 
